27f78e04ec316ff0a9a21dde33707ae5af7463d7
Al Elliott <adrelliott@gmail.com>
2022-09-22 10:01:05 +0200

    UI: Create dashboard stat cards

---
+++ resources/views/livewire/dashboard/stat-cards.blade.php
@@ -0,0 +1,27 @@
+<div class="stats stats-vertical lg:stats-horizontal shadow w-10/12 justify-items-center">
+  
+    <div class="stat">
+      <div class="stat-title">Contacts</div>
+      <div class="stat-value">{{ rand(22,100) }}</div>
+      <div class="stat-desc">Total contacts in database</div>
+    </div>
+    
+    <div class="stat">
+      <div class="stat-title">Prospects</div>
+      <div class="stat-value">{{ rand(15,80) }}</div>
+      <div class="stat-desc">Contacts who've been contacted</div>
+    </div>
+    
+    <div class="stat">
+      <div class="stat-title">Leads</div>
+      <div class="stat-value">{{ rand(10,50) }}</div>
+      <div class="stat-desc">Contacts who've replied</div>
+    </div>
+    
+    <div class="stat">
+      <div class="stat-title">Won</div>
+      <div class="stat-value">{{ rand(3,20) }}</div>
+      <div class="stat-desc">Contacts who've bought</div>
+    </div>
+    
+  </div>
